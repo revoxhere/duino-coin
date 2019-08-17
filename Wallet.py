5f7d4fdbe69f3ba42a9a8a2f7e3eb2b0a776f3c9
Robert Piotrowski <50244265+revoxhere@users.noreply.github.com>
2019-08-17 16:19:44 +0200

    Placeholder wallet using new server communication

--- Wallet.py
+++ Wallet.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
 
 ###########################################
-#   Duino-Coin wallet version 0.3 alpha   #
+#   Duino-Coin wallet version 0.4 alpha   #
 # https://github.com/revoxhere/duino-coin #
 #       copyright by revox 2019           #
 ###########################################
+
+# Wallet will be updated (ability to auto-login and send funds) in the future
 
 import time, socket, sys, os
 
 users = {}
 status = ""
-host = 'localhost' #server ip
+host = 'serveo.net' #server ip
 port = 14808 #server port
 s = socket.socket()
 
 print("*****Official duino-coin wallet*****")
 print("*************version 3**************")
 print("\n")
+
 try: #try to establish connection
     s.connect((host, port))
     print("Successfully connected to the wallet server!")
@@ -35,11 +38,7 @@ if welcome == "n": #login system
         print("Sent registration request...")
         print("If registration takes more than 5 seconds, please restart and try again!")
         if password == password1:
-            s.send(bytes('REGI' , encoding='utf8')) #send register request to server
-            time.sleep(0.1)
-            s.send(bytes(username, encoding='utf8'))
-            time.sleep(0.1)
-            s.send(bytes(password, encoding='utf8'))
+            s.send(bytes("REGI,"+username+","+password, encoding='utf8')) #send register request to server
             key = s.recv(2)
             key=key.decode()
             if key == "OK":
@@ -61,12 +60,7 @@ if welcome == "y":
         password = input("Password:")
         print("Sent login request...")
         print("If login takes more than 5 seconds, please restart and try again!")
-        time.sleep(0.1)
-        s.send(bytes('LOGI' , encoding='utf8')) #send login request to server
-        time.sleep(0.1)
-        s.send(bytes(username, encoding='utf8'))
-        time.sleep(0.1)
-        s.send(bytes(password, encoding='utf8'))
+        s.send(bytes("LOGI,"+username+","+password, encoding='utf8')) #send login request to server
         key = s.recv(2)
         key=key.decode()
         if key == "OK":
@@ -76,15 +70,14 @@ if welcome == "y":
             break
         if key == "NO":
             print(" ")
-            print("Invalid credentials! There might've been an error. Try again! If you don't have an account, restart and register.")
+            print("Invalid credentials! If you don't have an account, restart and register.")
             time.sleep(10)
             sys.exit()
 menu=True
 while menu:
     print("\n"*17) #for privacy - move the screen up a bit so password won't be seen
     time.sleep(0.1)
-    s.send(bytes('BALA' , encoding='utf8'))
-    time.sleep(0.1)
+    s.send(bytes("BALA", encoding='utf8'))
     balance = s.recv(32)
     balance=balance.decode()
     print("*****Official duino-coin WALLET client*****")
@@ -94,30 +87,8 @@ while menu:
     print(" You can send funds using option below.")
     print(" Current fee: 0%")
     print("\n"*5)
-    chose = input("Do you want to send funds to someone? y/n: ")
-    if chose == "y": #info gathering....
-        print("Your current account balance is", balance, ". How much funds do you want to send?")
-        amount = input("Enter a value: ")
-        if amount > balance:
-            print("You don't have that many funds!")
-            time.sleep(3)
-            sys.exit()
-        if amount < balance: 
-            print("Enter username of user to who you want to send", amount, "funds.")
-            name = input("Enter username: ")
-            s.send(bytes('SEND', encoding='utf8'))
-            time.sleep(0.2)
-            s.send(bytes(username, encoding='utf8'))
-            time.sleep(0.1)
-            s.send(bytes(name, encoding='utf8'))
-            time.sleep(0.1)
-            s.send(bytes(amount, encoding='utf8'))
-            time.sleep(0.1)
-            message = s.recv(48) #get message from the server
-            message=message.decode()
-            print(message)
-            print("\n")
-            time.sleep(3)
+    print(" Sending funds is currenty DISABLED. Try again later.")
+
 
 
  
