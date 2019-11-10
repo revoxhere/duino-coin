672132924f7490d2db5d1e2ffe540e103cd85ae7
Robert Piotrowski <50244265+revoxhere@users.noreply.github.com>
2019-11-10 19:43:54 +0100

    0.6.5.1

--- PC Miner.py
+++ PC Miner.py
@@ -1,10 +1,5 @@
-print("===========================================")
-print("    Duino-Coin PC miner version 0.6.5")
-print(" https://github.com/revoxhere/duino-coin")
-print("   copyright by MrKris7100 & revox 2019")
-print("===========================================\n")
-
-import socket, threading, time, random, hashlib, configparser, sys, datetime, os, pip, signal, time
+import socket, threading, time, random, hashlib, configparser, sys, datetime, os, signal, time
+from pip._internal import main
 from decimal import Decimal
 from pathlib import Path
 from signal import signal, SIGINT
@@ -13,7 +8,7 @@ try:
     from colorama import init, Fore, Back, Style
 except:
     print("Colorama is not installed. Installing it...")
-    pip.main(['install', '--user', 'colorama'])
+    main(['install', '--user', 'colorama'])
     print("\n")
 
 global shares, diff, last_hash_count, khash_count, hash_count, config, VER, connection_counter
@@ -40,12 +35,12 @@ def loadConfig():
 	global pool_address, pool_port, username, password, efficiency
 	if not Path("MinerConfig_0.6.5.ini").is_file():
 		print(Style.BRIGHT + "Initial configuration, you can edit 'MinerConfig_0.6.5.ini' file later.")
-		print(Style.RESET_ALL + "Don't have an account? Use " + Fore.YELLOW + "Wallet" + Style.RESET_ALL + " to register.\n")
-		pool_address = input(Style.RESET_ALL + "Enter pool adddress " + Fore.YELLOW + "(official: serveo.net): ")
-		pool_port = input(Style.RESET_ALL + "Enter pool port " + Fore.YELLOW + "(official: 14808): ")
-		username = input(Style.RESET_ALL + "Enter username (the one you used to register): " + Fore.YELLOW)
-		password = input(Style.RESET_ALL + "Enter password (the one you used to register): " + Fore.YELLOW)
-		efficiency = input(Style.RESET_ALL + "Enter mining intensity " + Fore.YELLOW + "(1-100)%: ")
+		print("Don't have an account? Use " + Fore.YELLOW + "Wallet" + " to register.\n")
+		pool_address = input("Enter pool adddress " + Fore.YELLOW + "(official: serveo.net): ")
+		pool_port = input("Enter pool port " + Fore.YELLOW + "(official: 14808): ")
+		username = input("Enter username (the one you used to register): " + Fore.YELLOW)
+		password = input("Enter password (the one you used to register): " + Fore.YELLOW)
+		efficiency = input("Enter mining intensity " + Fore.YELLOW + "(1-100)%: ")
 		config['miner'] = {"address": pool_address,
 		"port": pool_port,
 		"username": username,
@@ -69,11 +64,11 @@ def connect():
 			soc.connect((str(pool_address), int(pool_port)))
 			soc.settimeout(timeout)
 			now = datetime.datetime.now()
-			print(now.strftime(Style.DIM + "[%H:%M:%S] ") + Style.RESET_ALL + Fore.GREEN + "Connected to pool on tcp://"+pool_address+":"+pool_port)
+			print(now.strftime(Style.DIM + "[%H:%M:%S] ") + Fore.GREEN + "Connected to pool on tcp://"+pool_address+":"+pool_port)
 			break
 		except:
 			now = datetime.datetime.now()
-			print(now.strftime(Style.DIM + "[%H:%M:%S] ") + Style.RESET_ALL + Fore.RED + "Cannot connect to pool server. There is probably a server update going on. Retrying in 15 seconds...")
+			print(now.strftime(Style.DIM + "[%H:%M:%S] ") + Fore.RED + "Cannot connect to pool server. There is probably a server update going on. Retrying in 15 seconds...")
 			time.sleep(15)
 		time.sleep(0.025)
 
@@ -82,11 +77,11 @@ def checkVersion():
 		SERVER_VER = soc.recv(1024).decode()
 		if SERVER_VER == VER:
 			now = datetime.datetime.now()
-			print(now.strftime(Style.DIM + "[%H:%M:%S] ") + Style.RESET_ALL + Fore.GREEN + "Successfully checked if miner is up-to-date.")
+			print(now.strftime(Style.DIM + "[%H:%M:%S] ") + Fore.GREEN + "Successfully checked if miner is up-to-date.")
 		else:
 			now = datetime.datetime.now()
-			print(now.strftime(Style.DIM + "[%H:%M:%S] ") + Style.RESET_ALL + Fore.RED + "Miner is outdated, please download latest version from https://github.com/revoxhere/duino-coin/releases/")
-			print(now.strftime(Style.DIM + "[%H:%M:%S] ") + Style.RESET_ALL + Fore.RED + "Exiting in 15 seconds.")
+			print(now.strftime(Style.DIM + "[%H:%M:%S] ") + Fore.RED + "Miner is outdated, please download latest version from https://github.com/revoxhere/duino-coin/releases/")
+			print(now.strftime(Style.DIM + "[%H:%M:%S] ") + Fore.RED + "Exiting in 15 seconds.")
 			time.sleep(15)
 			sys.exit()
 	except:
@@ -102,11 +97,11 @@ def login():
 			resp = soc.recv(1024).decode()
 			if resp == "OK":
 				now = datetime.datetime.now()
-				print(now.strftime(Style.DIM + "[%H:%M:%S] ") + Style.RESET_ALL + Fore.GREEN + "Successfully logged in.")
+				print(now.strftime(Style.DIM + "[%H:%M:%S] ") + Fore.GREEN + "Successfully logged in.")
 				break
 			if resp == "NO":
 				now = datetime.datetime.now()
-				print(now.strftime(Style.DIM + "[%H:%M:%S] ") + Style.RESET_ALL + Fore.RED + "Error! Wrong credentials or account doesn't exist!\nIf you don't have an account, register using Wallet!\nExiting in 15 seconds.")
+				print(now.strftime(Style.DIM + "[%H:%M:%S] ") + Fore.RED + "Error! Wrong credentials or account doesn't exist!\nIf you don't have an account, register using Wallet!\nExiting in 15 seconds.")
 				soc.close()
 				time.sleep(15)
 				sys.exit()
@@ -118,7 +113,7 @@ def login():
 def mine():
 	global last_hash_count, hash_count, khash_count, efficiency
 	now = datetime.datetime.now()
-	print(now.strftime(Style.DIM + "[%H:%M:%S] ") + Style.RESET_ALL + Fore.GREEN + "Miner thread started, using 'SHA' algorithm.")
+	print(now.strftime(Style.DIM + "[%H:%M:%S] ") + Fore.GREEN + "Miner thread started, using 'SHA' algorithm.")
 	efficiency = 100 - int(efficiency)
 	efficiency = efficiency * 0.01
 	while True:
@@ -151,12 +146,12 @@ def mine():
 					if feedback == "GOOD":
 						now = datetime.datetime.now()
 						shares[0] = shares[0] + 1 # Share accepted = increment feedback shares counter by 1
-						print(now.strftime(Style.DIM + "[%H:%M:%S] ") + Style.RESET_ALL + Fore.GREEN + "accepted: " + str(shares[0]) + "/" + str(shares[0] + shares[1]) + Style.DIM + " (" + str(shares[0] / (shares[0] + shares[1]) * 100) + "%)," + Style.NORMAL + Fore.WHITE + " diff: " + str(diff) + ", " + Style.BRIGHT + str(khash_count) + " khash/s " + Style.BRIGHT + Fore.YELLOW + "(yay!!!)")
+						print(now.strftime(Style.DIM + "[%H:%M:%S] ") + Fore.GREEN + "accepted: " + str(shares[0]) + "/" + str(shares[0] + shares[1]) + Style.DIM + " (" + str(shares[0] / (shares[0] + shares[1]) * 100) + "%)," + Style.NORMAL + Fore.WHITE + " diff: " + str(diff) + ", " + Style.BRIGHT + str(khash_count) + " khash/s " + Style.BRIGHT + Fore.YELLOW + "(yay!!!)")
 						break
 					elif feedback == "BAD":
 						now = datetime.datetime.now()
 						shares[1] = shares[1] + 1 # Share rejected = increment bad shares counter by 1
-						print(now.strftime(Style.DIM + "[%H:%M:%S] ") + Style.RESET_ALL + Fore.RED + "rejected: " + str(shares[1]) + "/" + str(shares[1] + shares[1]) + Style.DIM + " (" + str(shares[0] / (shares[0] + shares[1]) * 100) + "%)," + Style.NORMAL + Fore.WHITE + " diff: " + str(diff) + ", " + Style.BRIGHT + str(khash_count) + " khash/s "  + Style.BRIGHT + Fore.RED + "(boo!!!)")
+						print(now.strftime(Style.DIM + "[%H:%M:%S] ") + Fore.RED + "rejected: " + str(shares[1]) + "/" + str(shares[1] + shares[1]) + Style.DIM + " (" + str(shares[0] / (shares[0] + shares[1]) * 100) + "%)," + Style.NORMAL + Fore.WHITE + " diff: " + str(diff) + ", " + Style.BRIGHT + str(khash_count) + " khash/s "  + Style.BRIGHT + Fore.RED + "(boo!!!)")
 						break
 					time.sleep(0.025) # Try again if no response
 				break
@@ -173,6 +168,11 @@ init(autoreset=True)
 hush()
 while True:
     signal(SIGINT, handler)
+    print(Fore.WHITE + "===========================================")
+    print("    Duino-Coin PC miner version 0.6.5")
+    print(" https://github.com/revoxhere/duino-coin")
+    print("   copyright by MrKris7100 & revox 2019")
+    print("===========================================\n")
     try:
             loadConfig() #Firstly, load configfile
     except:
@@ -195,4 +195,3 @@ while True:
             print(Style.BIRGHT + Fore.RED + "There was an error while mining. Restarting." + Style.RESET_ALL)
     print(Style.RESET_ALL)
     time.sleep(1)
-
