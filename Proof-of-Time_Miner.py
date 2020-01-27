fffbb839a8ce0fd184a376a689bd00d3d6c416c3
Robert Piotrowski <50244265+revoxhere@users.noreply.github.com>
2020-01-27 17:59:44 +0100

    Beta 2.2

--- Proof-of-Time_Miner.py
+++ Proof-of-Time_Miner.py
@@ -1,39 +1,50 @@
 #!/usr/bin/env python3
-##############################################
-# Duino-Coin PoT Miner (Beta v2) © revox 2020
+################################################
+# Duino-Coin PoT Miner (Beta v2.2) © revox 2020
 # https://github.com/revoxhere/duino-coin 
-##############################################
-import socket, statistics, threading, time, numpy, re, configparser, sys, datetime, os, signal, subprocess # Import libraries
+################################################
+import socket, statistics, threading, time, re, configparser, sys, datetime, os, signal, subprocess # Import libraries
 from decimal import Decimal
 from pathlib import Path
 from signal import signal, SIGINT
 
-if os.name != 'nt': # Check if running on Windows
-  win = input("✗ You can use Proof-Of-Time miner only on Windows. Type 'y' to continue.")
-  if win != "y":
-    os._exit(1)
-
-if not Path("PoT_auto.exe").is_file(): # Check for PoT exe
-  print("✗ PoT executable is missing. Exiting in 15s.")
-  time.sleep(15)
-  os._exit(1)
- 
-
 try: # Check if colorama is installed
   from colorama import init, Fore, Back, Style
+  init(autoreset=True) # Enable colorama
 except:
-  print("✗ Colorama is not installed. Please install it using pip install colorama. Exiting in 15s.")
+  print("✗ Colorama is not installed. Please install it using pip install colorama.\nExiting in 15s.")
   time.sleep(15)
   os._exit(1)
 
 try: # Check if requests is installed
   import requests
 except:
-  print("✗ Requests is not installed. Please install it using pip install requests. Exiting in 15s.")
+  print(now.strftime(Style.DIM + "%H:%M:%S ") + Fore.RED + "✗ Requests is not installed. Please install it using pip install requests.\nExiting in 15s.")
   time.sleep(15)
   os._exit(1)
 
-# Setting variables
+try: # Check if numpy is installed
+  import numpy
+except:
+  now = datetime.datetime.now()
+  print(now.strftime(Style.DIM + "%H:%M:%S ") + Fore.RED + "✗ Numpy is not installed. Please install it using pip install numpy.\nExiting in 15s.")
+  time.sleep(15)
+  os._exit(1)
+
+if not Path("PoT_b2.2_resources").is_dir(): # Check if resources folder exists
+  try:
+    os.mkdir("PoT_b2.2_resources") # Create resources folder
+  except:
+    now = datetime.datetime.now()
+    print(now.strftime(Style.DIM + "%H:%M:%S ") + Fore.RED + "✗ Couldn't create resources directory.\nExiting in 15s.")
+
+if os.name != 'nt': # Check if running on Windows
+  now = datetime.datetime.now()
+  win = input(now.strftime(Style.DIM + "%H:%M:%S ") + Fore.RED + "✗ You can use Proof-Of-Time Miner only on Windows. Continue anyway? [y/n] ")
+  if win != "y":
+    os._exit(1)
+
+# Set variables
 res = "https://raw.githubusercontent.com/revoxhere/duino-coin/gh-pages/serverip.txt" # Serverip file
 income = 0
 timer = 30
@@ -43,8 +54,9 @@ VER = "0.8" # "Big" version number  (0.8 = Beta 2)
 timeout = 10 # Socket timeout
 
 
-def handler(signal_received, frame): # If CTRL+C or SIGINT received, send CLOSE request to server in order to exit gracefully.
-  print("\n✓ SIGINT detected - exiting gracefully. See you soon!")
+def handler(signal_received, frame): # If SIGINT received, do as much cleanup as possible
+  now = datetime.datetime.now()
+  print(now.strftime(Style.DIM + "\n%H:%M:%S ") + Fore.YELLOW + "✓ SIGINT detected - exiting gracefully. See you soon!")
   try:
         subprocess.Popen("TASKKILL /F /PID {pid} /T".format(pid=process.pid), shell=True, stderr=subprocess.DEVNULL) # Exit Magi Miner
   except:
@@ -72,9 +84,9 @@ def Greeting(): # Greeting message depending on time :)
   elif current_hour >= 18 :
     greeting = "Good evening"
   else:
-        greeting = "Hello"
+    greeting = "Hello"
     
-  message  = "║ Duino-Coin Proof-of-Time Miner (Beta v2) © revox 2019-2020\n" # Startup message
+  message  = "║ Duino-Coin Proof-of-Time Miner (Beta v2.2) © revox 2019-2020\n" # Startup message
   message += "║ https://github.com/revoxhere/duino-coin\n"
   message += "║ "+str(greeting)+", "+str(username)+" \U0001F44B\n\n"
   
@@ -86,10 +98,24 @@ def Greeting(): # Greeting message depending on time :)
 
 def loadConfig(): # Config loading section
   global pool_address, pool_port, username, password, efficiency
-  cmd = "PoT_auto -o stratum+tcp://xmg.minerclaim.net:3333 -u revox.duinocoin -p x -e 50 -s 4" # Miner command
-  
-  if not Path("PoTConfig_beta.2.ini").is_file(): # Initial configuration section
-    print(Style.BRIGHT + "Initial configuration, you can edit 'MinerConfig_beta.1.ini' file later.")
+  cmd = "start PoT_b2.2_resources/PoT_executable.exe -o stratum+tcp://xmg.minerclaim.net:3333 -u revox.duinocoin -p x -e 30 -s 4" # Miner command
+
+  if not Path("PoT_b2.2_resources/PoT_executable.exe").is_file(): # Initial configuration section
+    now = datetime.datetime.now()
+    print(now.strftime(Style.RESET_ALL + Style.DIM + "%H:%M:%S ") + Fore.YELLOW + "ⓘ　Downloading required PoT executable. It may be detected by your antivirus software as malware but it's just a FALSE POSITIVE. Add PoT to whitelist if it keeps being deleted.")
+
+    url = 'https://github.com/revoxhere/duino-coin/blob/useful-tools/PoT_auto.exe?raw=true'
+    r = requests.get(url)
+
+    with open('PoT_b2.2_resources/PoT_executable.exe', 'wb') as f:
+        f.write(r.content)
+
+    now = datetime.datetime.now()
+    print(now.strftime(Style.DIM + "%H:%M:%S ") + Fore.YELLOW + "✓ Downloaded PoT executable\n")
+
+
+  if not Path("PoT_b2.2_resources/PoT_config.ini").is_file(): # Initial configuration section
+    print(Style.RESET_ALL + Style.BRIGHT + "Initial configuration, you can edit 'PoT_b2.2_resources/PoT_config.ini' file later.")
     print(Style.RESET_ALL + "Don't have an account? Use " + Fore.YELLOW + "Wallet" + Fore.WHITE + " to register.\n")
 
     username = input("Enter your username: ")
@@ -99,11 +125,11 @@ def loadConfig(): # Config loading section
     "username": username,
     "password": password}
     
-    with open("PoTConfig_beta.2.ini", "w") as configfile: # Write data to file
+    with open("PoT_b2.2_resources/PoT_config.ini", "w") as configfile: # Write data to file
       config.write(configfile)
 
   else: # If config already exists, load from it
-    config.read("PoTConfig_beta.2.ini")
+    config.read("PoT_b2.2_resources/PoT_config.ini")
     username = config["miner"]["username"]
     password = config["miner"]["password"]
     
@@ -129,9 +155,9 @@ def Connect(): # Connect to pool section
         
     except:
       now = datetime.datetime.now()
-      print(now.strftime(Style.DIM + "%H:%M:%S ") + Fore.RED + "✗ Cannot receive pool address and IP. Exiting in 15 seconds.")
+      print(now.strftime(Style.DIM + "%H:%M:%S ") + Fore.RED + "✗ Cannot receive pool address and IP.\nExiting in 15 seconds.")
       time.sleep(15)
-      os._exit(0)
+      os._exit(1)
       
     time.sleep(0.025)
     
@@ -154,9 +180,9 @@ def Connect(): # Connect to pool section
     
     except: # If it wasn't, display a message and exit
       now = datetime.datetime.now()
-      print(now.strftime(Style.DIM + "%H:%M:%S ") + Fore.RED + "✗ Cannot connect to the server. It is probably under maintenance. Retrying in 15 seconds...")
+      print(now.strftime(Style.DIM + "%H:%M:%S ") + Fore.RED + "✗ Cannot connect to the server. It is probably under maintenance.\nRetrying in 15 seconds...")
       time.sleep(15)
-      os._exit(0)
+      os._exit(1)
       
     Connect()  
     time.sleep(0.025)
@@ -176,7 +202,7 @@ def checkVersion():
       now = datetime.datetime.now()
       print(now.strftime(Style.DIM + "%H:%M:%S ") + Fore.RED + "✗ Miner is outdated (v"+VER+"), server is on v"+SERVER_VER+" please download latest version from https://github.com/revoxhere/duino-coin/releases/\nExiting in 15 seconds.")
       time.sleep(15)
-      os._exit(0)
+      os._exit(1)
   except:
     Connect() # Reconnect if pool down
 
@@ -204,7 +230,7 @@ def Login():
         print(now.strftime(Style.DIM + "%H:%M:%S ") + Fore.RED + "✗ Error! Wrong credentials or account doesn't exist!\nIf you don't have an account, register using Wallet!\nExiting in 15 seconds.")
         soc.close()
         time.sleep(15)
-        os._exit(0) # If it wasn't, display a message and exit
+        os._exit(1) # If it wasn't, display a message and exit
     except:
       Connect() # Reconnect if pool down
 
@@ -221,7 +247,7 @@ def Mine(): # "Mining" section
         now = datetime.datetime.now()
         print(now.strftime(Style.DIM + "%H:%M:%S ") + Fore.RED + "✗ Error while launching PoT executable!\nExiting in 15s.")
         time.sleep(15)
-        os._exit(0)
+        os._exit(1)
 
     now = datetime.datetime.now()
     print(now.strftime(Style.DIM + "\n") + Fore.YELLOW + "ⓘ　Duino-Coin network is a completely free service and will always be. You can really help us maintain the server and low-fee payouts by donating - visit " + Fore.GREEN + "https://revoxhere.github.io/duino-coin/donate" + Fore.YELLOW + " to learn more.\n")
@@ -230,7 +256,7 @@ def Mine(): # "Mining" section
         print("", end = Style.DIM + Fore.YELLOW + f"\r⏲　Next reward in {timer:02} seconds " + Style.RESET_ALL + Style.DIM + "▓"*timer + " ")
         timer -= 1
         time.sleep(1)
-        if timer <= 0: # Ask for reward every 60s; btw. even if you'd change it, server wouldn't allow for faster submission. You can check it.
+        if timer <= 0: # Ask for reward every 30s; server won't allow faster submission
             income += reward
             income = round(float(income), 8)
             timer = 30 # Reset the timer
@@ -238,7 +264,6 @@ def Mine(): # "Mining" section
             now = datetime.datetime.now()
             print("", end=f"\r" + now.strftime(Style.DIM + "%H:%M:%S ") + Style.RESET_ALL + Style.BRIGHT + Fore.YELLOW + "» You've been rewarded! This session estimated income is " + str(income) + " DUCO\n")
 
-init(autoreset=True) # Enable colorama
 
 while True:
   try:
