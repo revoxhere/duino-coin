c5059af20bd3fdb3cfe6ae7570a97efc8f243120
Robert Piotrowski <50244265+revoxhere@users.noreply.github.com>
2020-03-21 21:17:46 +0100

    Update Wallet.py

--- Wallet.py
+++ Wallet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #############################################
-# Duino-Coin Wallet (1.1) © revox 2020
+# Duino-Coin Wallet (1.2) © revox 2020
 # https://github.com/revoxhere/duino-coin 
 #############################################
 import time, socket, sys, os, subprocess, datetime, configparser, tkinter, getpass, platform, webbrowser, urllib.request, random # Import libraries
@@ -15,7 +15,6 @@ try:
 except:
     print("Requests is not installed. Install it with: pip install requests.\nExiting in 15s.")
     time.sleep(15)
-
 
 # Default colors
 colorA = "white" #white when in light mode
@@ -46,7 +45,7 @@ balance = 0
 background = ""
 newbalance = 0
 sending = 0
-VER = "1.1" # Version number
+VER = "1.2" # Version number
 xmgusd = 0.023119
 resources = "Wallet_"+str(VER)+"_resources/"
 debug += "Successfully set variables\n"
@@ -62,7 +61,6 @@ themes = [
 
 label = tkinter.Label(loadingScr, text = "Configuring theme..." + str(" ")*20, bg = str(colorA), fg = str(colorC), font=("Arial", 8)).place(relx = 0.01, rely = 0.8)  
 loadingScr.update()
-time.sleep(0.1)
 
 def setTheme():
   global colorA, colorB, colorC, background, debug, wallet, about
@@ -92,7 +90,7 @@ def setTheme():
     colorA = "white"
     colorC = "gray"
     colorHighlight = "#27ae60"
-    messagebox.showinfo("Information", "Please restart the Wallet to apply changes.\nThis versions' community background was made by Tirachard Kumtanom.") # Display info
+    messagebox.showinfo("Information", "Please restart the Wallet to apply changes.\nThis versions' community background was made by Andreea Ch.") # Display info
      
   if v.get() == "0":
     config['wallet'] = {"username": username,
@@ -226,7 +224,12 @@ def loginProtocol(): # First-time login protocol
     username = nameEL.get()
     password = pwordEL.get()
     
-    key = s.recv(3) # Server version
+    SERVER_VER = s.recv(3) # Server version
+    if VER != SERVER_VER:
+        serverMsg = tkinter.Tk()
+        serverMsg.withdraw()
+        messagebox.showwarning("Error","You're not using the latest wallet version, please install new version to be up-to-date.")        
+
     s.send(bytes("LOGI,"+str(username)+","+str(password), encoding='utf8')) # Send login request to server
     time.sleep(0.015)
     key = s.recv(2).decode()
@@ -296,6 +299,12 @@ def loadConfig(): # Load config protocol
 
     while True: # Receive server key
         time.sleep(0.025)
+        SERVER_VER = s.recv(3) # Server version
+        if VER != SERVER_VER:
+            serverMsg = tkinter.Tk()
+            serverMsg.withdraw()
+            messagebox.showwarning("Error","You're not using the latest wallet version, please install new version to be up-to-date.")
+
         key = s.recv(3).decode()
         debug += "Received server keys\n"
         if key == "OK":
@@ -303,7 +312,7 @@ def loadConfig(): # Load config protocol
             loadingScr.update()
             s.send(bytes("FROM," + "Wallet," + str(pcusername) + "," + str(publicip) + "," + str(platform), encoding="utf8")) # Send info to server about client
             debug += "Sent client information\n"
-            time.sleep(0.3)
+            time.sleep(0.1)
             
             s.send(bytes("STAT", encoding='utf8'))
             time.sleep(0.025)
@@ -316,7 +325,7 @@ def loadConfig(): # Load config protocol
             os._exit(0)
         if key == "NO":
             loadingScr.destroy()
-            messagebox.showerror("Error","Fatal error in configfile (Wallet_config.ini)!\nRemove it and restart the wallet.")
+            messagebox.showerror("Error","Error in configfile (Wallet_config.ini)!\nRemove it and restart the wallet.")
             os._exit(1)
         else:
             loadConfig()
@@ -341,7 +350,7 @@ def Send():
 
     signupButton = Button(send, text='Send funds', activebackground = str(colorHighlight), command = sendProtocol, bg = str(colorA), fg = str(colorB)).grid(row = 4, column = 1)
 
-    send.mainloop
+    send.mainloop()
 
 def sendProtocol():
     global receipent, amount, sendProtocol,  debug, balanceTimer
@@ -349,47 +358,42 @@ def sendProtocol():
     amount = amount.get()
     debug += "Started sending funds protocol\n"
     if amount == "" or receipent == "": # Check if input fields aren't empty
-        sendit = "NO"
         debug += "Empty data!\n"
         messagebox.showerror("Error!","Empty data fields!")
         send.destroy()
         
     else:
-        
         if amount.isupper() or amount.islower(): # Check if amount contains only numbers
-            sendit = "NO"
             debug += "Amount contains letters!\n"
             messagebox.showerror("Error!","Incorrect amount!")
             send.destroy()
             
         else:
-
             if amount == "0": # Check if amount isn't zero
-                sendit = "NO"
                 debug += "Amount is 0!\n"
                 messagebox.showerror("Error!","Incorrect amount!")
                 send.destroy()
                 
             else:
-                        
                 if receipent == username: # Check wheter sending to yourself
-                    sendit = "NO"
                     debug += "Can't send to yourself!\n"
                     messagebox.showerror("Error!","Wrong receipent!")
                     send.destroy()
                     
                 else: # Send funds
-        
                     debug += "Sending "+amount+" DUCO from: "+username+" to "+receipent+"\n"
                     balanceTimer.cancel()
+                    
                     try:
                         s.send(bytes("SEND,"+username+","+receipent+","+amount, encoding='utf8')) #send sending funds request to server
                     except:
                         debug += "Fatal error while sending. Probably timeout.\n"
+                        
                     time.sleep(0.025)
                     message = s.recv(128).decode('utf8')
-                    if message.isalpha(): # If we receive balance instead of feedback, read message once again
+                    while message.isalpha():
                         message = s.recv(128).decode('utf8')
+                        
                     debug += "Server returned: "+message+"\n"
                     messagebox.showinfo("Server message", message) # Display the server message
                     send.destroy()
@@ -400,6 +404,73 @@ def Receive(): #receiving funds help dialog
     except:
         messagebox.showinfo("Receive funds", "To receive funds, instruct others to send money to your username.")
 
+def passchgProtocol():
+    global oldPassword, newPassword, confPassword, passchg, debug, balanceTimer
+    oldPassword = oldPassword.get()
+    newPassword = newPassword.get()
+    confPassword = confPassword.get()
+    debug += "Started changing password funds protocol\n"
+    if oldPassword == "" or newPassword == "" or confPassword == "": # Check if input fields aren't empty
+        debug += "Empty data!\n"
+        messagebox.showerror("Error!","Empty data fields!")
+        passchg.destroy()
+        
+    else:
+        if newPassword != confPassword: # Check if passwords match
+            debug += "Passwords don't match!\n"
+            messagebox.showerror("Error!","New passwords don't match!")
+            passchg.destroy()
+            
+        else:
+            if newPassword == oldPassword: # Check wheter new password is different
+                debug += "New password can't be the same as old password!\n"
+                messagebox.showerror("Error!","New passwords is the same!")
+                passchg.destroy()
+                
+            else:   
+                debug += "Changing user password\n"
+                balanceTimer.cancel()
+                
+                try:
+                    s.send(bytes("CHGP,"+username+","+oldPassword+","+newPassword, encoding='utf8')) #send sending funds request to server
+                except:
+                    debug += "Fatal error while sending. Probably timeout.\n"
+                    
+                time.sleep(0.025)
+                message = s.recv(128).decode('utf8')
+                while message.isalpha():
+                    message = s.recv(128).decode('utf8')
+                    
+                debug += "Server returned: "+message+"\n"
+                messagebox.showinfo("Server message", message) # Display the server message
+                if "Success" in message:
+                    os.remove(str(resources) + "Wallet_config.ini")
+                passchg.destroy()
+
+def changePass():
+    global oldPassword, newPassword, confPassword, passchg
+    passchg = Tk() # Changing password window
+    passchg.resizable(False, False)
+    passchg.title('Change password')
+    try:
+        passchg.iconbitmap(str(resources) + "Wallet_icon.ico")
+    except:
+        pass # Icon won't work on linux
+    passchg.configure(background = str(colorA))
+    oldPassword = Label(passchg, text="Current password: ", fg = str(colorB), bg = str(colorA)).grid(row = 1, column = 0, sticky = W) 
+    newPassword = Label(passchg, text='New password: ', fg = str(colorB), bg = str(colorA)).grid(row = 2, column = 0, sticky = W)
+    confPassword = Label(passchg, text='Confirm new password: ', fg = str(colorB), bg = str(colorA)).grid(row = 3, column = 0, sticky = W)
+
+    oldPassword = Entry(passchg, fg = str(colorB), bg = str(colorA))
+    oldPassword.grid(row = 1, column = 1)
+    newPassword = Entry(passchg, fg = str(colorB), bg = str(colorA))
+    newPassword.grid(row = 2, column = 1)
+    confPassword = Entry(passchg, fg = str(colorB), bg = str(colorA))
+    confPassword.grid(row = 3, column = 1)
+
+    passchgButton = Button(passchg, text='Change password', activebackground = str(colorHighlight), command = passchgProtocol, bg = str(colorA), fg = str(colorB)).grid(row = 4, column = 1)
+
+    passchg.mainloop()
 
 def About():
     global about, debug
@@ -425,11 +496,11 @@ def About():
     if theme == "Community mode":
         v.set(int(2))
 
-    label = tkinter.Label(about, text = "Duino-Coin Wallet", font=("Verdana", 20, "bold"), fg = str(colorHighlight), bg = str(colorA)).pack()
-    label = tkinter.Label(about, text = "version "+str(VER), fg = str(colorB), bg = str(colorA)).pack()
-    tkinter.Button(about, text = "Duino-Coin GitHub page", activebackground = str(colorHighlight), height = 1, command = GitHub, width=35, fg = str(colorB), bg = str(colorA)).pack(pady=4)
-    tkinter.Button(about, text = "Duino-Coin Discord server", activebackground = str(colorHighlight), height = 1, command = Discord, width=35, fg = str(colorB), bg = str(colorA)).pack(pady=3)
+    label = tkinter.Label(about, text = "Duino-Coin Wallet "+str(VER), font=("Verdana", 20, "bold"), fg = str(colorHighlight), bg = str(colorA)).pack()
+    tkinter.Button(about, text = "Duino-Coin GitHub", activebackground = str(colorHighlight), height = 1, command = GitHub, width=35, fg = str(colorB), bg = str(colorA)).pack(pady=4)
+    tkinter.Button(about, text = "Duino-Coin Discord", activebackground = str(colorHighlight), height = 1, command = Discord, width=35, fg = str(colorB), bg = str(colorA)).pack(pady=3)
     tkinter.Button(about, text = "Support the project (Donate)", activebackground = str(colorHighlight), height = 1, command = Donate, width=35, fg = str(colorB), bg = str(colorA)).pack(pady=4)
+    tkinter.Button(about, text = "Change password", activebackground = str(colorHighlight), height = 1, command = changePass, width=35, fg = str(colorB), bg = str(colorA)).pack(pady=4)
 
     label = tkinter.Label(about, text = "", fg = str(colorB), bg = str(colorA)).pack()
 
@@ -487,7 +558,6 @@ def getBalance():
         oldcontent = (transactions).read()
         transactions.seek(0, 0)
 
-        
         balance = round(float(balance), 12) # Format balances
         balanceusd = round(float(balance) * float(ducousd), 8)
         ducousd = round(float(ducousd), 8)
@@ -585,7 +655,7 @@ def WalletWindow():
     tkinter.Button(wallet, text = "Send funds", command = Send, activebackground = str(colorHighlight), bg = str(colorA), fg = str(colorB), font=("Arial", 10), height = 1, width = 30).place(relx = 0.1, rely = 0.42)
     tkinter.Button(wallet, text = "Receive funds", command = Receive, activebackground = str(colorHighlight), bg = str(colorA), fg = str(colorB), font=("Arial", 10), height = 1, width = 30).place(relx = 0.1, rely = 0.53)
     tkinter.Button(wallet, text = "Exchange DUCO", command = Exchange, activebackground = str(colorHighlight), bg = str(colorA), fg = str(colorB), font=("Arial", 10), height = 1, width = 30).place(relx = 0.1, rely = 0.64)
-    tkinter.Button(wallet, text = "Settings & Credits", command = About, activebackground = str(colorHighlight), bg = str(colorA), fg = str(colorB), font=("Arial", 10), height = 1, width = 30).place(relx = 0.1, rely = 0.75)
+    tkinter.Button(wallet, text = "Advanced options", command = About, activebackground = str(colorHighlight), bg = str(colorA), fg = str(colorB), font=("Arial", 10), height = 1, width = 30).place(relx = 0.1, rely = 0.75)
 
     label = tkinter.Label(wallet, text = "2019-2020 Duino-Coin developers", bg = str(colorA), fg = str(colorC), font=("Arial", 8)).place(relx = 0.01, rely = 0.93)
     wallet.mainloop()
