9a87a2ec79b4db435e09fa6027aa39b99c68ae4e
Robert Piotrowski <50244265+revoxhere@users.noreply.github.com>
2020-04-24 21:59:11 +0200

    1.4 - completely new Arduino Code

--- Arduino_Code/Arduino_Code.ino
+++ Arduino_Code/Arduino_Code.ino
@@ -1,10 +1,10 @@
 //////////////////////////////////////////////////////////
-//  _____        _                    _____      _       
-// |  __ \      (_)                  / ____|    (_)      
-// | |  | |_   _ _ _ __   ___ ______| |     ___  _ _ __  
+//  _____        _                    _____      _
+// |  __ \      (_)                  / ____|    (_)
+// | |  | |_   _ _ _ __   ___ ______| |     ___  _ _ __
 // | |  | | | | | | '_ \ / _ \______| |    / _ \| | '_ \ 
 // | |__| | |_| | | | | | (_) |     | |___| (_) | | | | |
-// |_____/ \__,_|_|_| |_|\___/       \_____\___/|_|_| |_| 
+// |_____/ \__,_|_|_| |_|\___/       \_____\___/|_|_| |_|
 //  Arduino Code remastered - v1.4 © revox 2019-2020
 //  Distributed under MIT License
 //////////////////////////////////////////////////////////
@@ -16,24 +16,32 @@
 //  and navigate to Getting Started page. Happy mining!
 //////////////////////////////////////////////////////////
 
-// Include SHA library
+// Include crypto library
 #include "Hash.h"
+//#define LED_BUILTIN 16 // Uncomment this if your board has built-in led on non-standard pin (NodeMCU - 16 or 2)
 
 void setup() {
   Serial.begin(115200); // Open serial port
+  pinMode(LED_BUILTIN, OUTPUT); // Prepare built-in led pin as output
 }
 
 void loop() {
+  DUCOS1A(); // Run DUCO-S1A algorithm
+}
+
+void DUCOS1A() {
   if (Serial.available() > 0) { // Wait for serial to become available
     String hash = Serial.readStringUntil('\n'); // Read hash
     String job = Serial.readStringUntil('\n'); // Read job
     unsigned int diff = Serial.readStringUntil('\n').toInt(); // Read difficulty
-
     for (unsigned int iJob = 0; iJob < diff * 100 + 1; iJob++) { // Difficulty loop
-      yield(); // Let Arduino/ESP do background tasks - anti-watchdog
+      yield(); // Let Arduino/ESP do background tasks - else watchdog will trigger
       String result = sha1(String(hash) + String(iJob)); // Hash previous block hash and current iJob
       if (result == job) { // If result is found
         Serial.println(iJob); // Send result back to Arduino Miner
+        digitalWrite(LED_BUILTIN, HIGH);   // Turn on built-in led
+        delay(50); // Wait a bit
+        digitalWrite(LED_BUILTIN, LOW); // Turn off built-in led
         break; // Stop and wait for more work
       }
     }
