4be09a729641f38354b84f6ceff40f23c6023662
Robert Piotrowski <50244265+revoxhere@users.noreply.github.com>
2020-06-05 16:32:23 +0200

    Update Ruby_Miner.rb

--- Unofficial miners/Ruby_Miner.rb
+++ Unofficial miners/Ruby_Miner.rb
@@ -1,31 +1,45 @@
+#!/usr/bin/env ruby
+# Minimal version of Duino-Coin PC Miner, useful for developing own apps. Created by revox 2020
 require 'socket'
 require 'digest'
-puts "Duino-Coin minimal Ruby miner"
-# Very early work in progress, for now it can find only one share
-# Will finish it some time later
+require 'net/http'
+require 'json' # Only default ruby libraries
 
-username = "username"
-password = "password"
+username = "your username here" 
+password = "your password here"
 
-s = TCPSocket.open('0.tcp.ngrok.io', 12596)
-puts "Connected to server"
+$VERBOSE = nil # Disable debug output
+url = 'https://raw.githubusercontent.com/revoxhere/duino-coin/gh-pages/serverip.txt'
+uri = URI(url)
+response = Net::HTTP.get(uri) # Receive server IP and port
+response = response.split("\n")
+serverip = response[0]
+serverport = response[1]
 
-SERVER_VER = s.gets(3) # Read lines from socket
+s = TCPSocket.open(serverip, serverport) # Connect to the server
+SERVER_VER = s.gets(3) # Read server version
 puts "Server is on version " + SERVER_VER
 
-s.puts("LOGI,"+String(username)+","+String(password))
-FEEDBACK = s.gets(2)
-puts "Login feedback is " + FEEDBACK
+s.puts("LOGI,"+String(username)+","+String(password)) # Send login request
+FEEDBACK = s.gets(2) # Receive login feedback
+if FEEDBACK == "OK"
+	puts "Loged in"
+else
+	puts "Error loging in - check account credentials!"
+	exit # Exit if credentials are incorrect
+end
 
-s.write("JOB")
-job = s.read()
-puts job
-job = job.split(',')
-difficulty = job[2]
-for result in 0..100*Integer(difficulty)+1 do
-	sha1 = Digest::SHA1.hexdigest String(String(job[0])+String(result))
-	if sha1 == String(job[1])
-		s.write(result)
-		puts "Share feedback: "+String(FEED)
+while 1 # Mining loop
+	s.puts("JOB") # Send job request
+	job = s.read(86) # Read job
+	job = job.split(',') # Split into previous block hash, result hash and difficulty
+	difficulty = job[2]
+	for result in 0..100 * Integer(difficulty) + 1 do # Find the numeric result
+		sha1 = Digest::SHA1.hexdigest String(String(job[0])+String(result)) # By checking if last block hash + n is result hash
+		if sha1 == String(job[1]) # If result is found
+			s.write(result) # Send numeric result to the server
+			SHAREFEED = s.read(4) # And receive result feedback
+			puts "Accepted share " + String(result) + " (Difficulty " + String(difficulty) + ")"
+		end
 	end
 end
