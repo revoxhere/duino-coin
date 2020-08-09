54e53dff615c5f043ad978a3d01404d4b5db6055
Bilaboz <44091093+Bilaboz@users.noreply.github.com>
2020-08-09 22:13:37 +0200

    Update Arduino_Miner.py

--- Arduino_Miner.py
+++ Arduino_Miner.py
@@ -5,7 +5,7 @@
 # Distributed under MIT license
 # © Duino-Coin Community 2020
 ##########################################
-import socket, threading, time, random, re, subprocess, platform, getpass, configparser, sys, datetime, os # Import libraries
+import socket, statistics, threading, time, random, re, subprocess, hashlib, platform, getpass, configparser, sys, datetime, os # Import libraries
 from pathlib import Path
 from signal import signal, SIGINT
 
