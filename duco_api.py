6b41defcbc6a69bd1d0295e499b983e595ef884e
httsmvkcom <alenyshka81ivanyshka@gmail.com>
2020-12-16 21:01:20 +0300

    Added a new server IP

--- duco_api.py
+++ duco_api.py
@@ -28,7 +28,7 @@ def decode_soc_no_utf(rec):
 
 def GetDucoPrice():
     global ducofiat
-    jsonapi = get("http://163.172.179.54/api.json", data = None)
+    jsonapi = get("http://93.113.207.71/api.json", data = None)
     if jsonapi.status_code == 200:
         content = jsonapi.content.decode()
         contentjson = loads(content)
