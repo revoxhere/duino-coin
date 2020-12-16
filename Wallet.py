4bb7e726d9cd24f9571438e377aca9b003aa6b8c
httsmvkcom <alenyshka81ivanyshka@gmail.com>
2020-12-16 21:36:14 +0300

    I think it's better that way.

--- Wallet.py
+++ Wallet.py
@@ -50,7 +50,7 @@ with urlopen("https://raw.githubusercontent.com/revoxhere/duino-coin/gh-pages/se
 
 def GetDucoPrice():
 	global ducofiat
-	jsonapi = get("http://93.113.207.71/api.json", data = None)
+	jsonapi = get("https://raw.githubusercontent.com/revoxhere/duco-statistics/master/api.json", data = None)
 	if jsonapi.status_code == 200:
 		content = jsonapi.content.decode()
 		contentjson = loads(content)
