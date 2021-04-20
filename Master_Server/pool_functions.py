0d36747af168cf8adb66e19296a5af6af60d981d
Bilaboz <44091093+Bilaboz@users.noreply.github.com>
2021-04-20 20:24:47 +0200

    Added ram + cpu usage

--- Master_Server/pool_functions.py
+++ Master_Server/pool_functions.py
@@ -9,9 +9,9 @@ PoolVersion = 0.1
 def PoolList_NO_SEND():
     with sqlite3.connect(database, timeout=database_timeout) as conn:
         c2 = conn.cursor()
-        c2.execute('''CREATE TABLE IF NOT EXISTS PoolList(identifier TEXT, name TEXT, ip TEXT, port TEXT, Status TEXT, hidden TEXT)''')
+        c2.execute('''CREATE TABLE IF NOT EXISTS PoolList(identifier TEXT, name TEXT, ip TEXT, port TEXT, Status TEXT, hidden TEXT, cpu REAL, ram REAL)''')
 
-        c2.execute("SELECT name, ip, port, Status FROM PoolList WHERE hidden != 'ok'")
+        c2.execute("SELECT name, ip, port, Status, ram, cpu FROM PoolList WHERE hidden != 'ok'")
         info = c2.fetchall()
         info = (str(info)).replace('\n', '')
 
@@ -42,11 +42,11 @@ def PoolLoginAdd(connection, data, PoolPassword):
         with sqlite3.connect(database, timeout=database_timeout) as conn:
             c2 = conn.cursor()
             print("Debug 4")
-            c2.execute('''CREATE TABLE IF NOT EXISTS PoolList(identifier TEXT, name TEXT, ip TEXT, port TEXT, Status TEXT, hidden TEXT)''')
+            c2.execute('''CREATE TABLE IF NOT EXISTS PoolList(identifier TEXT, name TEXT, ip TEXT, port TEXT, Status TEXT, hidden TEXT, cpu REAL, ram REAL)''')
 
             c2.execute("SELECT COUNT(identifier) FROM PoolList WHERE identifier = ?", (poolID,))
             if (c2.fetchall()[0][0]) == 0:
-                c2.execute("INSERT INTO PoolList(identifier, name, ip, port, Status, hidden) VALUES(?, ?, ?, ?, ?, ?)",(poolID, poolName, poolHost, poolPort, "False", poolHidden))
+                c2.execute("INSERT INTO PoolList(identifier, name, ip, port, Status, hidden, cpu, ram) VALUES(?, ?, ?, ?, ?, ?, ?, ?)",(poolID, poolName, poolHost, poolPort, "False", poolHidden, 0, 0))
 
                 conn.commit()
                 send_data(data="LoginOK", connection=connection)
@@ -71,7 +71,7 @@ def PoolLoginRemove(connection, data, PoolPassword):
     if password == PoolPassword:
         with sqlite3.connect(database, timeout=database_timeout) as conn:
             c2 = conn.cursor()
-            c2.execute('''CREATE TABLE IF NOT EXISTS PoolList(identifier TEXT, name TEXT, ip TEXT, port TEXT, Status TEXT, hidden TEXT)''')
+            c2.execute('''CREATE TABLE IF NOT EXISTS PoolList(identifier TEXT, name TEXT, ip TEXT, port TEXT, Status TEXT, hidden TEXT, cpu REAL, ram REAL)''')
 
             c2.execute("SELECT COUNT(identifier) FROM PoolList WHERE identifier = ?", (poolID,))
             if (c2.fetchall()[0][0]) != 0:
@@ -107,7 +107,7 @@ class Pool_Function_class:
         if str(poolVersion_sent) == str(PoolVersion):
             with sqlite3.connect(database, timeout=database_timeout) as conn:
                 c2 = conn.cursor()
-                c2.execute('''CREATE TABLE IF NOT EXISTS PoolList(identifier TEXT, name TEXT, ip TEXT, port TEXT, Status TEXT, hidden TEXT)''')
+                c2.execute('''CREATE TABLE IF NOT EXISTS PoolList(identifier TEXT, name TEXT, ip TEXT, port TEXT, Status TEXT, hidden TEXT, cpu REAL, ram REAL)''')
 
                 c2.execute("SELECT COUNT(identifier) FROM PoolList WHERE identifier = ?", (self.poolID,))
                 if (c2.fetchall()[0][0]) == 0:
@@ -128,10 +128,12 @@ class Pool_Function_class:
 
         try:
             info = str(data[1])
-
             info = ast.literal_eval(info)
+            
             rewards = info['rewards']
             blocks_to_add = int(info['blocks']['blockIncrease'])
+            poolCpu = float(info['cpu'])
+            poolRam = float(info['ram'])
         except Exception as e:
             print(e)
             send_data(data=f"NO,Error: {e}", connection=self.connection)
@@ -144,6 +146,8 @@ class Pool_Function_class:
             datab = conn.cursor()
             for user in rewards.keys():
                 datab.execute("UPDATE Users set balance = balance + ?  where username = ?", (float(rewards[user]), user))
+                
+            datab.execute("UPDATE PoolList SET cpu = ?, ram = ? WHERE identifier = ?", (poolCpu, poolRam, self.poolID))
             conn.commit()
 
         # ============
@@ -166,7 +170,7 @@ class Pool_Function_class:
 
         with sqlite3.connect(database, timeout=database_timeout) as conn:
             c2 = conn.cursor()
-            c2.execute('''CREATE TABLE IF NOT EXISTS PoolList(identifier TEXT, name TEXT, ip TEXT, port TEXT, Status TEXT, hidden TEXT)''')
+            c2.execute('''CREATE TABLE IF NOT EXISTS PoolList(identifier TEXT, name TEXT, ip TEXT, port TEXT, Status TEXT, hidden TEXT, cpu REAL, ram REAL)''')
 
             c2.execute("SELECT COUNT(identifier) FROM PoolList WHERE identifier = ?", (poolID,))
             if (c2.fetchall()[0][0]) == 0:
