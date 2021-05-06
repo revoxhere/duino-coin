d27c03f067639b51d5211f08ff63a83f205cb3e6
joaquinbvw <jverastegui@tecnologiaperu.com>
2021-05-05 20:10:24 -0500

    Update ESP32_Code.ino
    - Adding OTA support, to program the ESP32 using the WiFi network. Just select the corresponding Network port for your Arduino as you do on a Serial port.
    - Including a way to disable the Serial interface with a dummy Serial class. The Serial interface usually drives some current and power so if you're not using it you can save energy. This is optional and the user will need to comment line 34 if he wants to disable the Serial interface.
    - Fixing some typos on Serial messages.
    - Using F() on some Serial messages to save some RAM memory, this macro tells the compiler to store the message on flash memory.
    - Incrementing the watchdog timer timeout for the OTA. Programming the ESP32 through OTA can be slower than Serial and needs some time, this can trigger the watchod timer so it needs a little bit more time.

--- ESP32_Code/ESP32_Code.ino
+++ ESP32_Code/ESP32_Code.ino
@@ -24,11 +24,32 @@ const char* password = "Your WiFi password";        // Change this to your WiFi 
 const char* ducouser = "Your Duino-Coin username";  // Change this to your Duino-Coin username
 const char* rigname  = "ESP32";                     // Change this if you want to display a custom rig name in the Wallet
 #define LED_BUILTIN     2                           // Change this if your board has built-in led on non-standard pin (NodeMCU - 16 or 2)
-#define WDT_TIMEOUT 50                              // Define watchdog timer seconds
+#define WDT_TIMEOUT 60                              // Define watchdog timer seconds
+
+//////////////////////////////////////////////////////////
+//  If you don't want to use the Serial interface comment
+//  the ENABLE_SERIAL definition line (#define ENABLE_SERIAL)
+//////////////////////////////////////////////////////////
+
+#define ENABLE_SERIAL
+#ifndef ENABLE_SERIAL
+// disable Serial output
+#define Serial DummySerial
+static class {
+public:
+    void begin(...) {}
+    void print(...) {}
+    void println(...) {}
+    void printf(...) {}
+} Serial;
+#endif
 
 #include <esp_task_wdt.h>                           //Include WDT libary
 #include "hwcrypto/sha.h" // Include hardware accelerated hashing library
 #include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
 TaskHandle_t WiFirec;
 TaskHandle_t Task1;
@@ -40,62 +61,70 @@ const int port = 2811;
 
 volatile int wifiStatus = 0;
 volatile int wifiPrev = WL_CONNECTED;
+volatile bool OTA_status = false;
 
 void WiFireconnect( void * pvParameters ) {
-  //wifiStatus = WiFi.status();
-  //wifiPrev = wifiStatus;
-  delay(1000);
   int n = 0;
+  unsigned long previousMillis = 0;
+  const long interval = 500;
   esp_task_wdt_add(NULL);
   for(;;) {
     wifiStatus = WiFi.status();
-    if ((wifiStatus == WL_CONNECTED)&&(wifiPrev != WL_CONNECTED)) {
+    ArduinoOTA.handle();
+    if (OTA_status)  // If the OTA is working then reset the watchdog.
       esp_task_wdt_reset();
-      Serial.println("\nConnected to WiFi!");
+    // check if WiFi status has changed.
+    if ((wifiStatus == WL_CONNECTED)&&(wifiPrev != WL_CONNECTED)) {
+      esp_task_wdt_reset(); // Reset watchdog timer
+      Serial.println(F("\nConnected to WiFi!"));
       Serial.println("Local IP address: " + WiFi.localIP().toString());
       Serial.println();
     }
     else if ((wifiStatus != WL_CONNECTED)&&(wifiPrev == WL_CONNECTED)) {
-      esp_task_wdt_reset();
-      Serial.println("\nWiFi disconnected!");
+      esp_task_wdt_reset(); // Reset watchdog timer
+      Serial.println(F("\nWiFi disconnected!"));
       WiFi.disconnect();
-      Serial.println("Scanning for WiFi networks");
+      Serial.println(F("Scanning for WiFi networks"));
       n = WiFi.scanNetworks();
-      Serial.println("Scan done");
+      Serial.println(F("Scan done"));
       if (n == 0) {
-          Serial.println("No networks found. Resetting ESP32.");
+          Serial.println(F("No networks found. Resetting ESP32."));
           esp_restart();
       } else {
           Serial.print(n);
-          Serial.println(" networks found");
+          Serial.println(F(" networks found"));
           for (int i = 0; i < n; ++i) {
               // Print SSID and RSSI for each network found
               Serial.print(i + 1);
-              Serial.print(": ");
+              Serial.print(F(": "));
               Serial.print(WiFi.SSID(i));
-              Serial.print(" (");
+              Serial.print(F(" ("));
               Serial.print(WiFi.RSSI(i));
-              Serial.print(")");
+              Serial.print(F(")"));
               Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
               delay(10);
           }
       }
-      esp_task_wdt_reset();
+      esp_task_wdt_reset(); // Reset watchdog timer
       Serial.println();
-      Serial.println("Please, check if your WiFi network is on the list and check if it's strong enough (>-90dBm).");
+      Serial.println(F("Please, check if your WiFi network is on the list and check if it's strong enough (greater than -90)."));
       Serial.println("ESP32 will reset itself after "+String(WDT_TIMEOUT)+" seconds if can't connect to the network");
       Serial.print("Connecting to: " + String(ssid));
       WiFi.reconnect();
     }
     else if ((wifiStatus == WL_CONNECTED)&&(wifiPrev == WL_CONNECTED)) {
-      esp_task_wdt_reset();
+      esp_task_wdt_reset(); // Reset watchdog timer
       delay(1000);
     }
-    if(wifiStatus != WL_CONNECTED) {
-        Serial.print(".");
+    else {
+       // Don't reset watchdog timer. If the timer gets to the timeout then it will reset the MCU
+      unsigned long currentMillis = millis();
+      if (currentMillis - previousMillis >= interval) {
+        previousMillis = currentMillis;
+        Serial.print(F("."));
+      }
     }
     wifiPrev = wifiStatus;
-    delay(500);
   }
 }
 
@@ -124,22 +153,24 @@ void Task1code( void * pvParameters ) {
   String feedback1 = "";
   esp_task_wdt_add(NULL);
   for(;;) {
+    if (OTA_status)  // If the OTA is working then reset the watchdog.
+      esp_task_wdt_reset();
     while(wifiStatus != WL_CONNECTED){
       delay(1000);
       esp_task_wdt_reset();
     }
     Shares1 = 0; // Share variable
-    Serial.println("\nCORE1 Connecting to Duino-Coin server...");
+    Serial.println(F("\nCORE1 Connecting to Duino-Coin server..."));
     // Use WiFiClient class to create TCP connection
     client1.setTimeout(1);
     client1.flush();
     yield();
     if (!client1.connect(host, port)) {
-      Serial.println("CORE1 connection failed");
+      Serial.println(F("CORE1 connection failed"));
       delay(500);
       continue;
     }
-    Serial.println("CORE1 is connected");
+    Serial.println(F("CORE1 is connected"));
     while(!client1.available()){
       yield();
       if (!client1.connected())
@@ -172,10 +203,10 @@ void Task1code( void * pvParameters ) {
       delay(50);
       yield();
       buff1size = client1.available();
-      Serial.print("CORE1 Buffer size is ");
+      Serial.print(F("CORE1 Buffer size is "));
       Serial.println(buff1size);
       if (buff1size<=10) {
-        Serial.println("CORE1 Buffer size is too small. Requesting another job.");
+        Serial.println(F("CORE1 Buffer size is too small. Requesting another job."));
         continue;
       }
       hash1 = client1.readStringUntil(','); // Read data to the first peroid - last block hash
@@ -210,19 +241,19 @@ void Task1code( void * pvParameters ) {
           ElapsedTimeSeconds1 = ElapsedTimeMiliSeconds1 / 1000; // Convert to seconds
           HashRate1 = iJob1 / ElapsedTimeSeconds1; // Calculate hashrate
           if (!client1.connected()) {
-            Serial.println("CORE1 Lost connection. Trying to reconnect");
+            Serial.println(F("CORE1 Lost connection. Trying to reconnect"));
             if (!client1.connect(host, port)) {
-              Serial.println("CORE1 connection failed");
+              Serial.println(F("CORE1 connection failed"));
               break;
             }
-            Serial.println("CORE1 Reconnection successful.");
+            Serial.println(F("CORE1 Reconnection successful."));
           }
           client1.flush();
           client1.print(String(iJob1) + "," + String(HashRate1) + ",ESP32 CORE1 Miner v2.3," + String(rigname)); // Send result to server
-          Serial.println("CORE1 Posting result and waiting for feedback.");
+          Serial.println(F("CORE1 Posting result and waiting for feedback."));
           while(!client1.available()){
             if (!client1.connected()) {
-              Serial.println("CORE1 Lost connection. Didn't receive feedback.");
+              Serial.println(F("CORE1 Lost connection. Didn't receive feedback."));
               break;
             }
             delay(10);
@@ -235,7 +266,7 @@ void Task1code( void * pvParameters ) {
           Shares1++;
           Serial.println("CORE1 " + String(feedback1) + " share #" + String(Shares1) + " (" + String(iJob1) + ")" + " Hashrate: " + String(HashRate1));
           if (HashRate1 < 4000) {
-            Serial.println("CORE1 Low hashrate. Restarting");
+            Serial.println(F("CORE1 Low hashrate. Restarting"));
             client1.flush();
             client1.stop();
             esp_restart();
@@ -244,7 +275,7 @@ void Task1code( void * pvParameters ) {
         }
       }
     }
-    Serial.println("CORE1 Not connected. Restarting core 1");
+    Serial.println(F("CORE1 Not connected. Restarting core 1"));
     client1.flush();
     client1.stop();
   }
@@ -275,23 +306,25 @@ void Task2code( void * pvParameters ) {
   String feedback = "";
   esp_task_wdt_add(NULL);
   for(;;) {
+    if (OTA_status)  // If the OTA is working then reset the watchdog.
+      esp_task_wdt_reset();
     while(wifiStatus != WL_CONNECTED){
       delay(1000);
       esp_task_wdt_reset();
     }
     Shares = 0; // Share variable
     
-    Serial.println("\nCORE2 Connecting to Duino-Coin server...");
+    Serial.println(F("\nCORE2 Connecting to Duino-Coin server..."));
     // Use WiFiClient class to create TCP connection
     client.setTimeout(1);
     client.flush();
     yield();
     if (!client.connect(host, port)) {
-      Serial.println("CORE2 connection failed");
+      Serial.println(F("CORE2 connection failed"));
       delay(500);
       continue;
     }
-    Serial.println("CORE2 is connected");
+    Serial.println(F("CORE2 is connected"));
     while(!client.available()){
       yield();
       if (!client.connected())
@@ -324,10 +357,10 @@ void Task2code( void * pvParameters ) {
       delay(50);
       yield();
       buff1size = client.available();
-      Serial.print("CORE1 Buffer size is ");
+      Serial.print(F("CORE2 Buffer size is "));
       Serial.println(buff1size);
       if (buff1size<=10) {
-        Serial.println("CORE1 Buffer size is too small. Requesting another job.");
+        Serial.println(F("CORE2 Buffer size is too small. Requesting another job."));
         continue;
       }
       hash = client.readStringUntil(','); // Read data to the first peroid - last block hash
@@ -362,19 +395,19 @@ void Task2code( void * pvParameters ) {
           ElapsedTimeSeconds = ElapsedTimeMiliSeconds / 1000; // Convert to seconds
           HashRate = iJob / ElapsedTimeSeconds; // Calculate hashrate
           if (!client.connected()) {
-            Serial.println("CORE1 Lost connection. Trying to reconnect");
+            Serial.println(F("CORE2 Lost connection. Trying to reconnect"));
             if (!client.connect(host, port)) {
-              Serial.println("CORE1 connection failed");
+              Serial.println(F("CORE2 connection failed"));
               break;
             }
-            Serial.println("CORE1 Reconnection successful.");
+            Serial.println(F("CORE2 Reconnection successful."));
           }
           client.flush();
           client.print(String(iJob) + "," + String(HashRate) + ",ESP32 CORE2 Miner v2.3," + String(rigname)); // Send result to server
-          Serial.println("CORE1 Posting result and waiting for feedback.");
+          Serial.println(F("CORE2 Posting result and waiting for feedback."));
           while(!client.available()){
             if (!client.connected()) {
-              Serial.println("CORE1 Lost connection. Didn't receive feedback.");
+              Serial.println(F("CORE2 Lost connection. Didn't receive feedback."));
               break;
             }
             delay(10);
@@ -387,7 +420,7 @@ void Task2code( void * pvParameters ) {
           Shares++;
           Serial.println("CORE2 " + String(feedback) + " share #" + String(Shares) + " (" + String(iJob) + ")" + " Hashrate: " + String(HashRate));
           if (HashRate < 4000) {
-            Serial.println("CORE2 Low hashrate. Restarting");
+            Serial.println(F("CORE2 Low hashrate. Restarting"));
             client.flush();
             client.stop();
             esp_restart();
@@ -396,7 +429,7 @@ void Task2code( void * pvParameters ) {
         }
       }
     }
-    Serial.println("CORE2 Not connected. Restarting core 2");
+    Serial.println(F("CORE2 Not connected. Restarting core 2"));
     client.flush();
     client.stop();
   }
@@ -408,7 +441,56 @@ void setup() {
   Serial.begin(500000); // Start serial connection
   Serial.println("\n\nDuino-Coin ESP32 Miner v2.3");
   WiFi.mode(WIFI_STA); // Setup ESP in client mode
+  btStop();
   WiFi.begin(ssid, password); // Connect to wifi
+  
+  OTA_status = false;
+  
+  // Port defaults to 3232
+  // ArduinoOTA.setPort(3232);
+  
+  // Hostname defaults to esp3232-[MAC]
+  // ArduinoOTA.setHostname("myesp32");
+  
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+  
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+  
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+      
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+      OTA_status = true;
+    })
+    .onEnd([]() {
+      Serial.println(F("\nEnd"));
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+      OTA_status = true;
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println(F("Auth Failed"));
+      else if (error == OTA_BEGIN_ERROR) Serial.println(F("Begin Failed"));
+      else if (error == OTA_CONNECT_ERROR) Serial.println(F("Connect Failed"));
+      else if (error == OTA_RECEIVE_ERROR) Serial.println(F("Receive Failed"));
+      else if (error == OTA_END_ERROR) Serial.println(F("End Failed"));
+      OTA_status = false;
+      esp_restart();
+    });
+  
+  ArduinoOTA.begin();
+  
   esp_task_wdt_init(WDT_TIMEOUT, true); // Init Watchdog timer
   pinMode(LED_BUILTIN,OUTPUT);
   
