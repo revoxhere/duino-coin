1aeba877e61e6513abe44c9c25c603c3e117cf8b
aadek112 <85436943+aadek112@users.noreply.github.com>
2021-06-06 18:23:05 +0200

    Same as AVR_Miner

--- PC_Miner.py
+++ PC_Miner.py
@@ -21,7 +21,7 @@ from re import sub
 from signal import SIGINT, signal
 from socket import socket
 from statistics import mean
-from subprocess import DEVNULL, Popen, check_call
+from subprocess import DEVNULL, Popen, check_call, call
 from threading import Thread as thrThread
 from time import ctime, sleep, strptime, time
 from multiprocessing import Lock
@@ -35,7 +35,8 @@ def install(package):
         pip.main(["install",  package])
     except AttributeError:
         check_call([sys.executable, '-m', 'pip', 'install', package])
-        execl(sys.executable, sys.executable, *sys.argv)
+
+    call([sys.executable, __file__])
 
 
 def now():
