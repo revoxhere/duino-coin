3e600d568b64100069057bf3ff7356d4effec3ca
joaquinbvw <jverastegui@tecnologiaperu.com>
2021-06-12 20:00:26 -0500

    Update types.c

--- Arduino_Code/sha1/types.c
+++ Arduino_Code/sha1/types.c
@@ -32,15 +32,11 @@ sha1_hasher_t sha1_hasher_new(void)
 
 void sha1_hasher_init(sha1_hasher_t hasher)
 {
-#ifdef __AVR__
 	uint8_t i;
 	for(i = 0; i < SHA1_HASH_LEN / 4; i++)
 	{
 		hasher->state.words[i] = pgm_read_dword(sha1_init_state + i);
 	}
-#else
-	memcpy(hasher->state.words, sha1_init_state, SHA1_HASH_LEN);
-#endif
 	hasher->block_offset = 0;
 	hasher->total_bytes = 0;
 	hasher->_lock = 0;
