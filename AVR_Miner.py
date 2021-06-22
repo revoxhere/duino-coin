829b8a4032e0c2c776b812b2838b6fb0ea769fce
HGEpro <57325674+HGEpro@users.noreply.github.com>
2021-06-22 15:20:13 +0200

    Fixed typo

--- AVR_Miner.py
+++ AVR_Miner.py
@@ -154,7 +154,7 @@ try:
         elif locale.startswith('tr'):
             lang = 'turkish'
         elif locale.startswith('pt'):
-            lang = 'portugese'
+            lang = 'portuguese'
         elif locale.startswith('zh'):
             lang = 'chinese_simplified'
         else:
