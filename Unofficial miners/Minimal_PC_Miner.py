28c0790dbb5a58c183f8dae4014824e79c89e79d
Robert Piotrowski <50244265+revoxhere@users.noreply.github.com>
2021-07-01 11:06:22 +0200

    Update Minimal_PC_Miner.py

--- Unofficial miners/Minimal_PC_Miner.py
+++ Unofficial miners/Minimal_PC_Miner.py
@@ -1,43 +1,42 @@
 #!/usr/bin/env python3
-# Minimal version of Duino-Coin PC Miner, useful for developing own apps. Created by revox 2020-2021
-# Modifications made by Robert Furr (robtech21)
+# Minimal version of Duino-Coin PC Miner, useful for developing own apps.
+# Created by revox 2020-2021
+# Modifications made by Robert Furr (robtech21) and YeahNotSewerSide
 
 import hashlib
 import os
 import socket
 import sys  # Only python3 included libraries
 import time
-import urllib.request
 import ssl
 import select
 
+AVAILABLE_PORTS = [2812, 2813, 2814, 2815, 2816]
 soc = None
-# soc = socket.socket()
-# soc.settimeout(10)
 
-username = input('Username?\n> ') 
-
-DiffChoice = input('Use lower difficulty? (Y/N) [Leave empty for default of True]\n> ')
-if DiffChoice.lower == "n":
+username = input('Username?\n> ')
+diff_choice = input(
+    'Use lower difficulty? (Y/N) [Leave empty for default of True]\n> ')
+if diff_choice.lower == "n":
     UseLowerDiff = False
 else:
     UseLowerDiff = True
 
-AVAILABLE_PORTS = [2812,2813,2814,2815,2816]
-def get_fastest_connection(server_ip:str):
+
+def get_fastest_connection(server_ip: str):
     connection_pool = []
     available_connections = []
     for i in range(len(AVAILABLE_PORTS)):
         connection_pool.append(socket.socket())
         connection_pool[i].setblocking(0)
         try:
-            connection_pool[i].connect((server_ip, 
+            connection_pool[i].connect((server_ip,
                                         AVAILABLE_PORTS[i]))
         except BlockingIOError as e:
             pass
-    
-    ready_connections,_,__ = select.select(connection_pool,[],[])
-    
+
+    ready_connections, _, __ = select.select(connection_pool, [], [])
+
     while True:
         for connection in ready_connections:
             try:
@@ -46,55 +45,21 @@ def get_fastest_connection(server_ip:str):
                 continue
             if server_version == b'':
                 continue
-               
+
             available_connections.append(connection)
             connection.send(b'PING')
-         
-        ready_connections,_,__ = select.select(available_connections,[],[])
+
+        ready_connections, _, __ = select.select(available_connections, [], [])
         ready_connections[0].recv(100)
-        # python is smart enough to close all other connections
-        # I hope
         ready_connections[0].settimeout(10)
         return ready_connections[0]
-            
-    
 
-def retrieve_server_ip():
-    print("> Retrieving Pool Address And Port")
-    pool_obtained = False
-    while not pool_obtained:
-        try:
-            ctx = ssl.create_default_context()
-            ctx.check_hostname = False
-            ctx.verify_mode = ssl.CERT_NONE
-            serverip = ("https://raw.githubusercontent.com/"
-                            + "revoxhere/"
-                            + "duino-coin/gh-pages/"
-                            + "serverip.txt")
-            with urllib.request.urlopen(serverip, context=ctx) as content:
-                # Read content and split into lines
-                content = content.read().decode().splitlines()
-            global pool_address, pool_port
-            # Line 1 = IP
-            pool_address = content[0]
-            # Line 2 = port
-            pool_port = content[1]
-            pool_obtained =  True
-        except:
-            print("> Failed to retrieve Pool Address and Port, Retrying.")
-            continue
 
-retrieve_server_ip()
 while True:
     try:
         print('Searching for fastest connection to the server')
-        soc = get_fastest_connection(str(pool_address))
+        soc = get_fastest_connection(str("server.duinocoin.com"))
         print('Fastest connection found')
-        
-        # This section connects and logs user to the server
-        # soc.connect((str(pool_address), int(pool_port)))
-        # server_version = soc.recv(3).decode()  # Get server version
-        #print("Server is on version", server_version)
 
         # Mining section
         while True:
@@ -117,14 +82,14 @@ while True:
             # Split received data to job and difficulty
             job = job.split(",")
             difficulty = job[2]
-            
+
             hashingStartTime = time.time()
             base_hash = hashlib.sha1(str(job[0]).encode('ascii'))
             temp_hash = None
-            
+
             for result in range(100 * int(difficulty) + 1):
                 # Calculate hash with difficulty
-                temp_hash =  base_hash.copy()
+                temp_hash = base_hash.copy()
                 temp_hash.update(str(result).encode('ascii'))
                 ducos1 = temp_hash.hexdigest()
 
@@ -167,6 +132,5 @@ while True:
 
     except Exception as e:
         print("Error occured: " + str(e) + ", restarting in 5s.")
-        retrieve_server_ip()
         time.sleep(5)
         os.execl(sys.executable, sys.executable, *sys.argv)
