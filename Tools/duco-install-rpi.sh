a4988cc47c9acbdaeef9f68453b2e837a5244ab5
Grantrocks <grantmac12345678911@outlook.com>
2022-02-11 17:34:58 -0500

    fixed the shortcuts with no icon

--- Tools/duco-install-rpi.sh
+++ Tools/duco-install-rpi.sh
@@ -9,30 +9,31 @@ if [ ! -d ~/duino-coin/ ];then
   git clone https://github.com/revoxhere/duino-coin # Clone Duino-Coin repository
 fi
 cd ~/duino-coin
+echo "Installing python packages"
 python3 -m pip install -r requirements.txt # Install pip dependencies
-#menu button
+
 if [ ! -f ~/.local/share/applications/duino-coin-pc-miner.desktop ];then
-  echo "Creating menu button..."
+  echo "Creating PC Miner button..."
 fi
 mkdir -p ~/.local/share/applications
 echo "[Desktop Entry]
 Name=Duino Coin PC Miner
 Comment=Duino Coin miner for the raspberry pi
 Exec=python3 /home/pi/duino-coin/PC_Miner.py
-Icon=cd/home/pi/resources/PCMiner.png
+Icon=/home/pi/duino-coin/Resources/PCMiner.png
 Terminal=true
 Type=Application
 Categories=Utility;
 StartupNotify=true" > ~/.local/share/applications/duino-coin-pc-miner.desktop
 
 if [ ! -f ~/.local/share/applications/duino-coin-avr.desktop ];then
-  echo "Creating Settings menu button..."
+  echo "Creating AVR Miner menu button..."
 fi
 echo "[Desktop Entry]
 Name=Duino Coin AVR Miner
 Comment=AVR miner for duino coin.
 Exec=python3 /home/pi/duino-coin/AVR_Miner.py
-Icon=home/pi/duino-coin/resources/AVRMiner.png
+Icon=/home/pi/duino-coin/Resources/AVRMiner.png
 Terminal=true
 Type=Application
 Categories=Utility;
