3785a0a7988937f39cb9da51c6a6eca89008d802
Grantrocks <grantmac12345678911@outlook.com>
2022-02-13 15:16:41 -0500

    beutified the script a bit

--- Tools/duco-install-rpi.sh
+++ Tools/duco-install-rpi.sh
@@ -1,20 +1,33 @@
 #!/bin/sh
+
 echo "Installing Duino Coin On Your Raspberry Pi"
 sleep 1
 sudo apt update
 sudo apt install python3 python3-pip git python3-pil python3-pil.imagetk -y # Install dependencies
 cd
-if [ ! -d ~/duino-coin/ ];then
-  echo "Duino Coin not yet downloaded. Downloading...."
-  git clone https://github.com/revoxhere/duino-coin # Clone Duino-Coin repository
+
+# Update if duino con exists
+if [ -d ~/duino-coin/ ];then
+  cd ~/duino-coin
+  git pull
 fi
+
+if [ ! -d ~/duino-coin/ ];then
+    echo "Duino Coin not yet downloaded. Downloading...."
+    git clone https://github.com/revoxhere/duino-coin # Clone Duino-Coin repository
+fi
+
 cd ~/duino-coin
 echo "Installing python packages"
 python3 -m pip install -r requirements.txt # Install pip dependencies
 
+#Check if the app exists
 if [ ! -f ~/.local/share/applications/duino-coin-pc-miner.desktop ];then
-  echo "Creating PC Miner button..."
+    echo "Creating PC Miner button..."
 fi
+
+# Shortcut For PC Miner
+
 mkdir -p ~/.local/share/applications
 echo "[Desktop Entry]
 Name=Duino Coin PC Miner
@@ -26,9 +39,12 @@ Type=Application
 Categories=Utility;
 StartupNotify=true" > ~/.local/share/applications/duino-coin-pc-miner.desktop
 
+# Check if the app exists
 if [ ! -f ~/.local/share/applications/duino-coin-avr.desktop ];then
-  echo "Creating AVR Miner menu button..."
+    echo "Creating AVR Miner menu button..."
 fi
+#Create The Shortcut For Avr Miner
+
 echo "[Desktop Entry]
 Name=Duino Coin AVR Miner
 Comment=AVR miner for duino coin.
