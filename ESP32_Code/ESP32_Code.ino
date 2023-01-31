17ed5734082eac03c7d055f721a31e72b2d76e49
Aleks Racheev <fixxer@inbox.ru>
2023-01-31 23:15:18 +0100

    Disable IOT by default

--- ESP32_Code/ESP32_Code.ino
+++ ESP32_Code/ESP32_Code.ino
@@ -112,7 +112,7 @@ typedef struct {
 
 // Uncomment the line below if you wish to use an BMP280 sensor (Duino IoT beta)
 #define USE_BMP280
-#ifdef USE_BMP280
+//#ifdef USE_BMP280
 // Install "Adafruit BMP280 Library" if you get an error
 # include <Adafruit_BMP280.h>
 # include <Wire.h>
