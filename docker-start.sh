ca0feeba4be312fa874a40773b569d0e61659f2c
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-11-21 02:56:46 -0600

    Update docker-start.sh

--- docker-start.sh
+++ docker-start.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-echo '[PC Miner]' > /app/duino-coin/Settings.cfg
-echo "username = ${DUCO_USERNAME}" >> /app/duino-coin/Settings.cfg
-echo "mining_key = $(echo -n ${DUCO_MINING_KEY} | base64)" >> /app/duino-coin/Settings.cfg
-echo "intensity = ${DUCO_INTENSITY}" >> /app/duino-coin/Settings.cfg
-echo "threads = ${DUCO_THREADS}" >> /app/duino-coin/Settings.cfg
-echo "start_diff = ${DUCO_START_DIFF}" >> /app/duino-coin/Settings.cfg
-echo "donate = ${DUCO_DONATE}" >> /app/duino-coin/Settings.cfg
-echo "identifier = ${DUCO_IDENTIFIER}" >> /app/duino-coin/Settings.cfg
-echo "algorithm = ${DUCO_ALGORITHM}" >> /app/duino-coin/Settings.cfg
-echo "language = ${DUCO_LANGUAGE}" >> /app/duino-coin/Settings.cfg
-echo "soc_timeout = ${DUCO_SOC_TIMEOUT}" >> /app/duino-coin/Settings.cfg
-echo "report_sec = ${DUCO_REPORT_SEC}" >> /app/duino-coin/Settings.cfg
-echo "raspi_leds = ${DUCO_RASPI_LEDS}" >> /app/duino-coin/Settings.cfg
-echo "raspi_cpu_iot = ${DUCO_RASPI_CPU_IOT}" >> /app/duino-coin/Settings.cfg
-echo "discord_rp = ${DUCO_DISCORD_RP}" >> /app/duino-coin/Settings.cfg
+# Set the base path without the version number
+base_path="/app/duino-coin/Duino-Coin PC Miner"
 
-python3 /app/duino-coin/PC_Miner.py
+# Find the highest version number in the folder
+highest_version=$(ls -d "${base_path}"/*/ | grep -Eo '[0-9.]+' | sort -Vr | head -n 1)
+
+# Construct the full path with the highest version number
+full_path="${base_path} ${highest_version}"
+
+echo '[PC Miner]' > "${full_path}/Miner_config.cfg"
+echo "username = ${DUCO_USERNAME}" >> "${full_path}/Miner_config.cfg"
+echo "mining_key = $(echo -n ${DUCO_MINING_KEY} | base64)" >> "${full_path}/Miner_config.cfg"
+echo "intensity = ${DUCO_INTENSITY}" >> "${full_path}/Miner_config.cfg"
+echo "threads = ${DUCO_THREADS}" >> "${full_path}/Miner_config.cfg"
+echo "start_diff = ${DUCO_START_DIFF}" >> "${full_path}/Miner_config.cfg"
+echo "donate = ${DUCO_DONATE}" >> "${full_path}/Miner_config.cfg"
+echo "identifier = ${DUCO_IDENTIFIER}" >> "${full_path}/Miner_config.cfg"
+echo "algorithm = ${DUCO_ALGORITHM}" >> "${full_path}/Miner_config.cfg"
+echo "language = ${DUCO_LANGUAGE}" >> "${full_path}/Miner_config.cfg"
+echo "soc_timeout = ${DUCO_SOC_TIMEOUT}" >> "${full_path}/Miner_config.cfg"
+echo "report_sec = ${DUCO_REPORT_SEC}" >> "${full_path}/Miner_config.cfg"
+echo "raspi_leds = ${DUCO_RASPI_LEDS}" >> "${full_path}/Miner_config.cfg"
+echo "raspi_cpu_iot = ${DUCO_RASPI_CPU_IOT}" >> "${full_path}/Miner_config.cfg"
+echo "discord_rp = ${DUCO_DISCORD_RP}" >> "${full_path}/Miner_config.cfg"
+
+chmod +x "${full_path}/PC_Miner.py"
+python3 "${full_path}/PC_Miner.py"
