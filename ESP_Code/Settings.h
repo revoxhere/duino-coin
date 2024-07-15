5587e975a350a9874e9b43160e73a6d1bc0b4be4
revoxhere <50244265+revoxhere@users.noreply.github.com>
2024-07-15 21:59:33 +0200

    SSD1306 support

--- ESP_Code/Settings.h
+++ ESP_Code/Settings.h
@@ -34,6 +34,14 @@ extern const char PASSWORD[] = "PASSW0RD";
 // ESP8266 WDT loop watchdog. Do not edit this value, but if you must - do not set it too low or it will falsely trigger during mining!
 #define LWD_TIMEOUT 30000
 
+// Uncomment to enable a SSD1306 OLED screen on the I2C bus to display mining info in real time
+// Default connections (can be overriden with Wire.begin(SDA, SCL)):
+// GND - GND
+// VCC - 5V or 3.3V depending on display
+// SCL - GPIO21 (ESP32) or GPIO5 (ESP8266) or GPIO35 (ESP32-S2)
+// SDA - GPIO22 (ESP32) or GPIO4 (ESP8266) or GPIO33 (ESP32-S2)
+// #define DISPLAY_SSD1306
+
 // -------------------------------------------------------------- //
 
 // ---------------------- IoT examples -------------------------- //
@@ -72,8 +80,10 @@ extern const char PASSWORD[] = "PASSW0RD";
 extern unsigned int hashrate = 0;
 extern unsigned int difficulty = 0;
 extern unsigned long share_count = 0;
+extern unsigned long accepted_share_count = 0;
 extern String node_id = "";
 extern String WALLET_ID = "";
+extern unsigned int ping = 0;
 
 #if defined(USE_INTERNAL_SENSOR)
   #include "driver/temp_sensor.h"
@@ -99,6 +109,13 @@ extern String WALLET_ID = "";
   #define DHTTYPE DHT11
   
   DHT extern dht(DHTPIN, DHTTYPE);
+#endif
+
+#if defined(DISPLAY_SSD1306)
+    #include <U8g2lib.h>
+    #include <Wire.h>
+    // Display definition from the U8G2 library. Edit if you use a different display
+    U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
 #endif
 
 #endif  // End of SETTINGS_H
