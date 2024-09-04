c2d6c0324cf075a1e1e3b6d157157ac4c576fc16
jpx13 <jp.xiume@aliceadsl.fr>
2024-09-04 10:24:34 +0200

    commented lines that were forgotten

--- ESP_Code/User_Setup.h
+++ ESP_Code/User_Setup.h
@@ -213,7 +213,7 @@
 
 // For ESP32 Dev board (7735 tft)
 // The hardware SPI can be mapped to any pins
-// only one display type should be uncommented
+// ONLY ONE DISPLAY TYPE SHOULD BE UNCOMMENTED
 
 // FOR TFT 7735 Display uncomment the following lines
 
@@ -221,19 +221,16 @@
 #define TFT_SCLK 18
 #define TFT_CS   15  // Chip select control pin
 #define TFT_DC   16  // Data Command control pin
-
-
 #define TFT_RST  4  // Reset pin (could connect to RST pin)
 
 // FOR Lilygo TT-GO 1.14 uncomment the following lines
 
-#define TFT_MOSI 19
-#define TFT_SCLK 18
-#define TFT_CS   5  // Chip select control pin
-#define TFT_DC   16  // Data Command control pin
-#define TFT_BL   4  // LED back-light
-
-#define TFT_RST  23  // Reset pin (could connect to RST pin)
+//#define TFT_MOSI 19
+//#define TFT_SCLK 18
+//#define TFT_CS   5  // Chip select control pin
+//#define TFT_DC   16  // Data Command control pin
+//#define TFT_BL   4  // LED back-light
+//#define TFT_RST  23  // Reset pin (could connect to RST pin)
 
 // For ESP32 Dev board (only tested with GC9A01 display)
 // The hardware SPI can be mapped to any pins
